5eb20526b2ae811d9c07ba6f41db7c491b1e1355
iks <iks@plugnine.com>
2023-09-06 15:13:59 +0900

    switch function

--- index.php
+++ index.php
@@ -8,48 +8,54 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <style type="text/css">
     .row div{border:1px solid grey}
+    .redbox{background-color: red;}
 </style>
-<!--
-2칸
-1:11
-2:10
-~11:1
-3칸
-4칸
-5칸
-6칸
-SSL php
--->
-<?
-// 단열 주석
-/* 다열주석 */
-// 변수 $변수명
-// 문자열 합치기는 .이다
-// 변수 형이 없다
-// 반복문 for(증감변수초기화;증감변수조건;증감){코드블럭} while
-for($i=1;$i<7;$i++){
-    $a='h'.$i;
-    print('<'.$a.'>'.$i.'안녕하세요</'.$a.'>');
-}
-?>
     </head>
     <body>
-
-        
         <?php
-        for($wkan=2;$wkan<7;$wkan++){
+        // function 함수명( 매개변수){코드블럭;return 변수명}
+        function makeBcol($wkan=3){
             //$wkan=3; // 나누고 싶은칸수
-
             $total=12; // 부트스트랩 전체컬럼수
-            $cNo=($total/$wkan); // 클래스 숫자
+            /* php 수학 내장함수  */
+            //$cNo=ceil($total/$wkan); // 올림 클래스 숫자
+            //$cNo=floor($total/$wkan); // 내림
+
+            $cNo=round($total/$wkan); // 내림
             $cnt=$total/$cNo; // 반복숫자
             print('<div class="row">'."\n");
             for($i=0;$i<$cnt;$i++){
-                $s='<div class="col-lg-'.$cNo.'"><h1>.col-md-'.$cNo.'</h1></div>'."\n";
+                if($i %2==1){
+                    $addClass='redbox';
+                }else{
+                    $addClass='';
+                }
+                $s='<div class="col-lg-'.$cNo.' '.$addClass.'"><h1>.col-md-'.$cNo.'('.$cnt.')</h1></div>'."\n";
                 print($s);
             } //2nd for
             print('</div>'."\n");
+        }
+
+        for($kan=2;$kan<7;$kan++){
+            makeBcol($kan);
         }//1st for
+        $score=75;
+        $score=10*floor($score/10);
+        print($score);
+        switch($score){
+            case 90:
+                print('수');
+                break;
+            case 80:
+                print('우');
+                break;
+            case 70:
+                print('미');
+                break;
+            default:
+                print('낙제');             
+        }
+
         ?>
         
        
