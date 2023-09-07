73bb60402dd1d8405864d69cd61f6e33f47309be
iks <iks@plugnine.com>
2023-09-07 11:33:08 +0900

    sql1

--- index.php
+++ index.php
@@ -13,14 +13,16 @@
     </head>
     <body>
         <?php
+        // http://127.0.0.1/myadmin/ root apmsetup
+        // 데이터베이스생성
         // def 함수명():
-        //선언 function 함수명( 매개변수){코드블럭;return 변수명}
+        // 선언 function 함수명( 매개변수){코드블럭;return 변수명}
         function makeBcol($wkan=3){
-            //$wkan=3; // 나누고 싶은칸수
+            // $wkan=3; // 나누고 싶은칸수
             $total=12; // 부트스트랩 전체컬럼수
             /* php 수학 내장함수  */
-            //$cNo=ceil($total/$wkan); // 올림 클래스 숫자
-            //$cNo=floor($total/$wkan); // 내림
+            // $cNo=ceil($total/$wkan); // 올림 클래스 숫자
+            // $cNo=floor($total/$wkan); // 내림
 
             $cNo=round($total/$wkan); // 내림
             $cnt=$total/$cNo; // 반복숫자
