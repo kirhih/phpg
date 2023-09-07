c91c4d4ec54ca117e0b75658f256d74249ba9672
iks <iks@plugnine.com>
2023-09-07 14:25:40 +0900

    page

--- index.php
+++ index.php
@@ -12,7 +12,11 @@
 </style>
     </head>
     <body>
+    <div class="panel panel-default">
+        <div class="panel-body">
         <?php
+        include('./module/navbar.php');
+        include('./module/pagehead.php');
         // http://127.0.0.1/myadmin/ root apmsetup
         // 데이터베이스생성
         // 데이터베이스 연결
@@ -38,5 +42,10 @@
         print('</tr>');
         print('</table>');
         ?>
+        </div>
+    </div>
+    <?php
+    //include('./module/footer.php');
+    ?>
     </body>
 </html>
