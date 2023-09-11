d914e2bacd5773eb6e33287959108fed01e07c32
iks <iks@plugnine.com>
2023-09-11 12:29:04 +0900

    ppython

---
+++ python/main1.py
@@ -0,0 +1,20 @@
+
+#%%
+# 모듈화
+# 직접 불러서 네이밍
+import prs.prs99 as pp
+pp.make99()
+# %%
+# 폴더 안에서 파일을 불러서 네이밍
+from prs import prs99 as p9
+p9.make99()
+
+# %%
+from prs.prs99 import *
+make99()
+person()
+# %%
+#not good
+import main as m
+m.make99()
+# %%
