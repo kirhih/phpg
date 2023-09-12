c8f7de2f45e7e725114288e48b314465966ec1fc
iks <iks@plugnine.com>
2023-09-12 17:34:39 +0900

    mlt

--- python/ML/mlbasic.py
+++ python/ML/mlbasic.py
@@ -32,9 +32,23 @@ plt.hist(irtgt)
 df['tgt']=irtgt
 df
 # %%
-sns.pairplot(df,hue='tgt')
+sns.pairplot(df,hue='tgt',palette='winter')
 # %%
+plt.plot(irtgt)
+#%%
 from sklearn.neighbors import KNeighborsClassifier
-knn3=KNeighborsClassifier(n_neighbors=3)
+from sklearn.model_selection import train_test_split # 시험문제 분리하기
+from sklearn.metrics import accuracy_score # 정확도 계산하기
+X_train,X_test,Y_train,Y_test=train_test_split(irdata,irtgt,test_size=0.3,shuffle=True,random_state=1)
+print(X_train.shape,X_test.shape)
+print(Y_train.shape,Y_test.shape)
 
+#%%
+knn3=KNeighborsClassifier(n_neighbors=3) # 모델 지정하기
+knn3.fit(X_train,Y_train) # 학습시키기
+pred=knn3.predict(X_test) #시험보기
+print(pred) # 시험본답 출력
+print(Y_test) # 실제답
+acc=accuracy_score(pred,Y_test)
+print("점수",acc)
 # %%
