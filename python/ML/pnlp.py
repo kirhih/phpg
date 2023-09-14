7e47f8c98fde0e9b08e3b080da14db24a7063350
iks <iks@plugnine.com>
2023-09-14 16:30:21 +0900

    cbow

--- python/ML/pnlp.py
+++ python/ML/pnlp.py
@@ -14,13 +14,68 @@ kkma=Kkma()
 res=kkma.pos('안녕하세요 그런데 여러분 만나서 반갑습니다.')
 res
 # %%
-reqPos=['NNG','NNP','NP','VV','VA','VCP','VCN','JC','MAC','EFN','EFA','EFQ','EFO','EFA','EFI','EFR']
-wset=[]
-for r in res:
-    if(r[1] in reqPos):
-        wset.append(r[0])
-        print(r)
-print(wset)
-
+def getPOS(txt='안녕하세요 그런데 여러분 만나서 반갑습니다.'):
+    res=kkma.pos(txt)
+    reqPos=['NNG','NNP','NP','VV','VA','VCN','JC','MAC','EFA','EFQ','EFO','EFA','EFI','EFR']#,'VCP','EFN'
+    wset=[]
+    for r in res:
+        if(r[1] in reqPos):
+            wset.append(r[0])
+            #print(r)
+    return(' '.join(wset))
+getPOS()
 
 # %%
+fname='../src/현진건-운수_좋은_날+B3356-개벽.txt'
+with open(fname,encoding='utf8') as f:
+    r=f.readlines()
+print(r)    
+# %%
+lucky=''.join(r)
+lucky[:100]
+# %%
+lucky=lucky.replace('\n\n','{nn}')
+lucky=lucky.replace('\n','')
+lucky=lucky.replace('{nn}','.')
+print(lucky)
+
+# %%
+lucks=lucky.split('.')
+lucks[:10]
+# %%
+copus=[]
+for luck in lucks:
+    ltxt=getPOS(luck)
+    copus.append(ltxt)
+copus[:10]
+
+# %%
+#t='새침하게 흐린 품이 눈이 올 듯하더니 눈은 아니 오고 얼다가 만 비가 추적추적 내리는 날이었다'
+#getPOS(t)
+# %%
+# 단어행렬 CBOW
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+cvect=CountVectorizer()
+cvfit=cvect.fit_transform(copus)
+cvtable=cvfit.toarray()
+print(cvtable.shape)
+print(cvect.vocabulary_)
+# %%
+plt.imshow(cvtable[:100,:100])
+# %%
+# 빈도-역빈도 TFIDF
+tvect=TfidfVectorizer()
+tvfit=tvect.fit_transform(copus)
+tvtable=tvfit.toarray()
+print(tvtable.shape)
+print(tvect.vocabulary_)
+# %%
+plt.imshow(tvtable[:100,:100])
+# %%
+cdf=pd.DataFrame(cvtable[:100,:100])
+cdf
+# %%
+tdf=pd.DataFrame(tvtable[:100,:100])
+tdf
+# %%
+
