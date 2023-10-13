77413d42e51d0702c849e2769a49410992931b43
iks <iks@plugnine.com>
2023-10-13 12:16:01 +0900

    and3

--- android/app/src/main/java/com/asiae/ai23/MainActivity.kt
+++ android/app/src/main/java/com/asiae/ai23/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.asiae.ai23
 
+import android.R
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -17,14 +19,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.asiae.ai23.ui.theme.AI23Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,18 +45,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun ImgCard(){
-    Card {
-
-        Box(modifier = Modifier.width(100.dp).padding(0.dp).border(1.dp, Color.Black).padding(10.dp)) {
-
+fun ImgCard(i:Int=1){
+    var name="m"+i.toString()
+    //Log.e(name)
+    val context = LocalContext.current
+    val drawableId = remember(name) {
+        context.resources.getIdentifier(
+            name,
+            "drawable",
+            context.packageName
+        )
+    }
+    Card (modifier = Modifier
+        .padding(10.dp)
+        .border(0.dp, Color.White)
+        .padding(5.dp)){
+        Box(modifier = Modifier.width(100.dp)) {
             Image(
-                painter = painterResource(id = R.drawable.m1),
+                painter = painterResource(id = drawableId),
                 contentDescription = "musical1"
             )
             Box(modifier = Modifier.align(Alignment.BottomCenter)) {
                 Text(
-                    text = "안녕하세요",
+                    text = "안녕하세요 "+i.toString(),
                     color = Color.White,
                     textAlign = TextAlign.Center,
                     modifier = Modifier.fillMaxWidth()
@@ -64,11 +80,12 @@ fun ImgCard(){
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column {
-        Row {
-            ImgCard()
-            ImgCard()
-            ImgCard()
+    Column (modifier = Modifier.padding(5.dp)){
+        Row (){
+            for (i in 1..3){
+                ImgCard(i)
+            }
+
         }
         Row {
             ImgCard()
