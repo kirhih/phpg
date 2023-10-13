cd2958a716fed75915704443f02e94af002b50f6
iks <iks@plugnine.com>
2023-10-13 11:27:57 +0900

    and

---
+++ android/app/src/main/java/com/asiae/ai23/Main2Activity.kt
@@ -0,0 +1,11 @@
+package com.asiae.ai23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
